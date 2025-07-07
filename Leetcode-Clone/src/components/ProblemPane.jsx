df0101bccaee8b1e3b42e73d7670151cd13426f1
safeer <safeer997@gmail.com>
2025-07-07 22:40:27 +0530

    finish mvp product

--- Leetcode-Clone/src/components/ProblemPane.jsx
+++ Leetcode-Clone/src/components/ProblemPane.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../styles/ProblemPane.css';
 
 const ProblemPane = () => {
-  return <div>ProblemPane</div>;
+  return <div className='problem-pane-container'>Question Statement</div>;
 };
 
 export default ProblemPane;
